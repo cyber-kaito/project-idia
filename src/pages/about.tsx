789a0f77513bccebc56a3decd4e15470feda22e1
cyberkaito <alexisaj12348@gmail.com>
2023-09-04 16:51:05 -0500

    feat: about page update

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -6,11 +6,29 @@ export default function Page() {
          <Title>About Project Idia</Title>
          <Paper radius="xs" className="p-4">
             <Text>
-               Project Idia is a resource website for Twisted Wonderland. Disney
-               Twisted-Wonderland is a registered trademark of Disney
-               Enterprises, Inc. This repo is not affiliated with The Walt
-               Disney Company, Aniplex, Inc., or Aniplex of America, Inc. All
-               game resources are copyrighted to the respective owners.
+               Project Idia is a resource website for Twisted Wonderland. This
+               website is still in development! Planned features include a team
+               builder and the ability to view information on past events and
+               gacha banners. If you have any suggestions or need to report an
+               issue/bug, please fill out the form linked below!
+            </Text>
+            <Text className="font-bold">
+               Important note: currently Project Idia only supports content
+               released in the English version of the game. JP server content
+               will be added in a future update.
+            </Text>
+         </Paper>
+
+         <Title className="pt-4">Contact Form</Title>
+         <Paper radius="xs" className="p-4">
+            <Text
+               component="a"
+               href="https://forms.gle/pres8rmwcQ2DT6NB8"
+               target="_blank"
+               rel="noopener noreferrer"
+               className="underline"
+            >
+               Link
             </Text>
          </Paper>
 
