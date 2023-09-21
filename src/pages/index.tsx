9fdc0691415877ea9fd19428c7fa4713e8ddc998
cyberkaito <alexisaj12348@gmail.com>
2023-09-20 22:49:32 -0500

    refactor: commemnted out functions im not using

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ import {
 } from "@mantine/core";
 import { useEffect, useState } from "react";
 import { getAssetURL } from "../utils";
-import alchemy_data from "../data/alchemy.json";
+// import alchemy_data from "../data/alchemy.json";
 // import gauntlet_data from "../data/gauntlet.json";
 import { TwitterTimelineEmbed } from "react-twitter-embed";
 
@@ -56,223 +56,223 @@ import { TwitterTimelineEmbed } from "react-twitter-embed";
 //    }
 // }
 
-function AlchemyBooks({ setNumber }: { setNumber: number }) {
-   const element = alchemy_data.books[setNumber - 1].element;
-   return (
-      <Center>
-         <Grid columns={3} className="px-20">
-            <Grid.Col span={1} className="flex justify-end">
-               <img
-                  src={getAssetURL(
-                     `images_webp/ui/items/magic_${element}.webp`
-                  )}
-                  alt={element.concat(" ", "book (small)")}
-                  loading="lazy"
-                  decoding="async"
-               ></img>
-            </Grid.Col>
-            <Grid.Col span={1} className="flex justify-center">
-               <img
-                  src={getAssetURL(
-                     `images_webp/ui/items/magic_${element}_p.webp`
-                  )}
-                  alt={element.concat(" ", "book (medium)")}
-                  loading="lazy"
-                  decoding="async"
-               ></img>
-            </Grid.Col>
-            <Grid.Col span={1} className="flex justify-start">
-               <img
-                  src={getAssetURL(
-                     `images_webp/ui/items/magic_${element}_pp.webp`
-                  )}
-                  alt={element.concat(" ", "book (large)")}
-                  loading="lazy"
-                  decoding="async"
-               ></img>
-            </Grid.Col>
-         </Grid>
-      </Center>
-   );
-}
+// function AlchemyBooks({ setNumber }: { setNumber: number }) {
+//    const element = alchemy_data.books[setNumber - 1].element;
+//    return (
+//       <Center>
+//          <Grid columns={3} className="px-20">
+//             <Grid.Col span={1} className="flex justify-end">
+//                <img
+//                   src={getAssetURL(
+//                      `images_webp/ui/items/magic_${element}.webp`
+//                   )}
+//                   alt={element.concat(" ", "book (small)")}
+//                   loading="lazy"
+//                   decoding="async"
+//                ></img>
+//             </Grid.Col>
+//             <Grid.Col span={1} className="flex justify-center">
+//                <img
+//                   src={getAssetURL(
+//                      `images_webp/ui/items/magic_${element}_p.webp`
+//                   )}
+//                   alt={element.concat(" ", "book (medium)")}
+//                   loading="lazy"
+//                   decoding="async"
+//                ></img>
+//             </Grid.Col>
+//             <Grid.Col span={1} className="flex justify-start">
+//                <img
+//                   src={getAssetURL(
+//                      `images_webp/ui/items/magic_${element}_pp.webp`
+//                   )}
+//                   alt={element.concat(" ", "book (large)")}
+//                   loading="lazy"
+//                   decoding="async"
+//                ></img>
+//             </Grid.Col>
+//          </Grid>
+//       </Center>
+//    );
+// }
 
-function GauntletRewards() {
-   return (
-      <Center>
-         <Table>
-            <thead>
-               <tr>
-                  <th>Location </th>
-                  <th>Rewards</th>
-               </tr>
-            </thead>
-            <tbody>
-               <tr>
-                  <td>Woods Behind Campus</td>
-                  <td className="flex justify-center">
-                     <img
-                        src={getAssetURL(
-                           `images_webp/ui/items/material_10001.webp`
-                        )}
-                        alt="Cosmic book (medium)"
-                        loading="lazy"
-                        decoding="async"
-                        className="w-1/4"
-                     ></img>
-                  </td>
-               </tr>
-               <tr>
-                  <td>Library</td>
-                  <td className="flex justify-center">
-                     <img
-                        src={getAssetURL(
-                           `images_webp/ui/items/material_10009.webp`
-                        )}
-                        alt="Cosmic book (medium)"
-                        loading="lazy"
-                        decoding="async"
-                        className="w-1/4"
-                     ></img>
-                  </td>
-               </tr>
-               <tr>
-                  <td>Sports Field</td>
-                  <td className="flex justify-center">
-                     <img
-                        src={getAssetURL(
-                           `images_webp/ui/items/material_10007.webp`
-                        )}
-                        alt="Cosmic book (medium)"
-                        loading="lazy"
-                        decoding="async"
-                        className="w-1/4"
-                     ></img>
-                     <img
-                        src={getAssetURL(
-                           `images_webp/ui/items/material_20002.webp`
-                        )}
-                        alt="Cosmic book (medium)"
-                        loading="lazy"
-                        decoding="async"
-                        className="w-1/4"
-                     ></img>
-                  </td>
-               </tr>
-               <tr>
-                  <td>Hall of Mirrors</td>
-                  <td className="flex justify-center">
-                     <img
-                        src={getAssetURL(
-                           `images_webp/ui/items/material_10006.webp`
-                        )}
-                        alt="Cosmic book (medium)"
-                        loading="lazy"
-                        decoding="async"
-                        className="w-1/4"
-                     ></img>
-                     <img
-                        src={getAssetURL(
-                           `images_webp/ui/items/material_20003.webp`
-                        )}
-                        alt="Cosmic book (medium)"
-                        loading="lazy"
-                        decoding="async"
-                        className="w-1/4"
-                     ></img>
-                  </td>
-               </tr>
-               <tr>
-                  <td>Botanical Garden</td>
-                  <td className="flex justify-center">
-                     <img
-                        src={getAssetURL(
-                           `images_webp/ui/items/material_10002.webp`
-                        )}
-                        alt="Cosmic book (medium)"
-                        loading="lazy"
-                        decoding="async"
-                        className="w-1/4"
-                     ></img>
-                     <img
-                        src={getAssetURL(
-                           `images_webp/ui/items/material_20001.webp`
-                        )}
-                        alt="Cosmic book (medium)"
-                        loading="lazy"
-                        decoding="async"
-                        className="w-1/4"
-                     ></img>
-                  </td>
-               </tr>
-               <tr>
-                  <td>Mystery Shop</td>
-                  <td className="flex justify-center">
-                     <img
-                        src={getAssetURL(
-                           `images_webp/ui/items/material_10008.webp`
-                        )}
-                        alt="Cosmic book (medium)"
-                        loading="lazy"
-                        decoding="async"
-                        className="w-1/4"
-                     ></img>
-                     <img
-                        src={getAssetURL(
-                           `images_webp/ui/items/material_20005.webp`
-                        )}
-                        alt="Cosmic book (medium)"
-                        loading="lazy"
-                        decoding="async"
-                        className="w-1/4"
-                     ></img>
-                  </td>
-               </tr>
-               <tr>
-                  <td>Coliseum</td>
-                  <td className="flex justify-center">
-                     <img
-                        src={getAssetURL(
-                           `images_webp/ui/items/material_20004.webp`
-                        )}
-                        alt="Cosmic book (medium)"
-                        loading="lazy"
-                        decoding="async"
-                        className="w-1/4"
-                     ></img>
-                     <img
-                        src={getAssetURL(
-                           `images_webp/ui/items/material_30012.webp`
-                        )}
-                        alt="Cosmic book (medium)"
-                        loading="lazy"
-                        decoding="async"
-                        className="w-1/4"
-                     ></img>
-                     <img
-                        src={getAssetURL(
-                           `images_webp/ui/items/material_30011.webp`
-                        )}
-                        alt="Cosmic book (medium)"
-                        loading="lazy"
-                        decoding="async"
-                        className="w-1/4"
-                     ></img>
-                     <img
-                        src={getAssetURL(
-                           `images_webp/ui/items/material_30010.webp`
-                        )}
-                        alt="Cosmic book (medium)"
-                        loading="lazy"
-                        decoding="async"
-                        className="w-1/4"
-                     ></img>
-                  </td>
-               </tr>
-            </tbody>
-         </Table>
-      </Center>
-   );
-}
+// function GauntletRewards() {
+//    return (
+//       <Center>
+//          <Table>
+//             <thead>
+//                <tr>
+//                   <th>Location </th>
+//                   <th>Rewards</th>
+//                </tr>
+//             </thead>
+//             <tbody>
+//                <tr>
+//                   <td>Woods Behind Campus</td>
+//                   <td className="flex justify-center">
+//                      <img
+//                         src={getAssetURL(
+//                            `images_webp/ui/items/material_10001.webp`
+//                         )}
+//                         alt="Cosmic book (medium)"
+//                         loading="lazy"
+//                         decoding="async"
+//                         className="w-1/4"
+//                      ></img>
+//                   </td>
+//                </tr>
+//                <tr>
+//                   <td>Library</td>
+//                   <td className="flex justify-center">
+//                      <img
+//                         src={getAssetURL(
+//                            `images_webp/ui/items/material_10009.webp`
+//                         )}
+//                         alt="Cosmic book (medium)"
+//                         loading="lazy"
+//                         decoding="async"
+//                         className="w-1/4"
+//                      ></img>
+//                   </td>
+//                </tr>
+//                <tr>
+//                   <td>Sports Field</td>
+//                   <td className="flex justify-center">
+//                      <img
+//                         src={getAssetURL(
+//                            `images_webp/ui/items/material_10007.webp`
+//                         )}
+//                         alt="Cosmic book (medium)"
+//                         loading="lazy"
+//                         decoding="async"
+//                         className="w-1/4"
+//                      ></img>
+//                      <img
+//                         src={getAssetURL(
+//                            `images_webp/ui/items/material_20002.webp`
+//                         )}
+//                         alt="Cosmic book (medium)"
+//                         loading="lazy"
+//                         decoding="async"
+//                         className="w-1/4"
+//                      ></img>
+//                   </td>
+//                </tr>
+//                <tr>
+//                   <td>Hall of Mirrors</td>
+//                   <td className="flex justify-center">
+//                      <img
+//                         src={getAssetURL(
+//                            `images_webp/ui/items/material_10006.webp`
+//                         )}
+//                         alt="Cosmic book (medium)"
+//                         loading="lazy"
+//                         decoding="async"
+//                         className="w-1/4"
+//                      ></img>
+//                      <img
+//                         src={getAssetURL(
+//                            `images_webp/ui/items/material_20003.webp`
+//                         )}
+//                         alt="Cosmic book (medium)"
+//                         loading="lazy"
+//                         decoding="async"
+//                         className="w-1/4"
+//                      ></img>
+//                   </td>
+//                </tr>
+//                <tr>
+//                   <td>Botanical Garden</td>
+//                   <td className="flex justify-center">
+//                      <img
+//                         src={getAssetURL(
+//                            `images_webp/ui/items/material_10002.webp`
+//                         )}
+//                         alt="Cosmic book (medium)"
+//                         loading="lazy"
+//                         decoding="async"
+//                         className="w-1/4"
+//                      ></img>
+//                      <img
+//                         src={getAssetURL(
+//                            `images_webp/ui/items/material_20001.webp`
+//                         )}
+//                         alt="Cosmic book (medium)"
+//                         loading="lazy"
+//                         decoding="async"
+//                         className="w-1/4"
+//                      ></img>
+//                   </td>
+//                </tr>
+//                <tr>
+//                   <td>Mystery Shop</td>
+//                   <td className="flex justify-center">
+//                      <img
+//                         src={getAssetURL(
+//                            `images_webp/ui/items/material_10008.webp`
+//                         )}
+//                         alt="Cosmic book (medium)"
+//                         loading="lazy"
+//                         decoding="async"
+//                         className="w-1/4"
+//                      ></img>
+//                      <img
+//                         src={getAssetURL(
+//                            `images_webp/ui/items/material_20005.webp`
+//                         )}
+//                         alt="Cosmic book (medium)"
+//                         loading="lazy"
+//                         decoding="async"
+//                         className="w-1/4"
+//                      ></img>
+//                   </td>
+//                </tr>
+//                <tr>
+//                   <td>Coliseum</td>
+//                   <td className="flex justify-center">
+//                      <img
+//                         src={getAssetURL(
+//                            `images_webp/ui/items/material_20004.webp`
+//                         )}
+//                         alt="Cosmic book (medium)"
+//                         loading="lazy"
+//                         decoding="async"
+//                         className="w-1/4"
+//                      ></img>
+//                      <img
+//                         src={getAssetURL(
+//                            `images_webp/ui/items/material_30012.webp`
+//                         )}
+//                         alt="Cosmic book (medium)"
+//                         loading="lazy"
+//                         decoding="async"
+//                         className="w-1/4"
+//                      ></img>
+//                      <img
+//                         src={getAssetURL(
+//                            `images_webp/ui/items/material_30011.webp`
+//                         )}
+//                         alt="Cosmic book (medium)"
+//                         loading="lazy"
+//                         decoding="async"
+//                         className="w-1/4"
+//                      ></img>
+//                      <img
+//                         src={getAssetURL(
+//                            `images_webp/ui/items/material_30010.webp`
+//                         )}
+//                         alt="Cosmic book (medium)"
+//                         loading="lazy"
+//                         decoding="async"
+//                         className="w-1/4"
+//                      ></img>
+//                   </td>
+//                </tr>
+//             </tbody>
+//          </Table>
+//       </Center>
+//    );
+// }
 
 // idea:
 // make 2 "cycle" variables, one for the charas and one for the books
